7b5e8899ca3e3f9d974625b1f9425c43fa1f194f
julia rodriguez <juliarodriguez@julia-rodriguezs-MacBook-Pro.local>
2019-06-18 10:52:28 -0600

    Views created

---
+++ PowerNapTimer/View Controlers/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  PowerNapTimer
+//
+//  Created by julia rodriguez on 6/18/19.
+//  Copyright © 2019 julia rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var napButton: UIButton!
+    
+    let timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    func updateLabel() {
+        
+    }
+    func updateButton() {
+        
+    }
+    
+    func startTimer() {
+        
+    }
+    func stopTimer() {
+        
+    }
+    func setTimer() {
+        
+    }
+    
+}
+
